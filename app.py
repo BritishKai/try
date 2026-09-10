d7b21a8f1784fd65566714a851aab4b3b029737a
BritishKai <sekarwidelia@gmail.com>
2026-09-10 16:21:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from streamlit_autorefresh import st_autorefresh
 wedding_date = date(1999, 9, 11)
 quiz_open = datetime(2026, 9, 12, 13, 00)
 
-if datetime.now() < quiz_open:
-    st.warning("Check your time!")
-    if st.button("Back to main page"):
-        st.switch_page("app.py")
-    st.stop()
-
 st.set_page_config(
     page_title="𑣲⋆ 27th Anniversary",
     layout="centered"
