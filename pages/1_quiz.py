9a382ba9646a6a376398ff5c759f68649ec660cf
BritishKai <sekarwidelia@gmail.com>
2026-09-10 18:42:41 +0700

    Update 1_quiz.py

--- pages/1_quiz.py
+++ pages/1_quiz.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import date, datetime
 
-quiz_open = datetime(2026, 9, 12, 13, 00)
+quiz_open = datetime(2026, 9, 10, 11, 43)
 
 if datetime.now() < quiz_open:
     st.warning("Check your time!")
