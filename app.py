ef36067c7c794aff72ab82a6b7d09668b95ace10
BritishKai <sekarwidelia@gmail.com>
2026-09-10 20:25:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime, date
 from streamlit_autorefresh import st_autorefresh
 
 wedding_date = date(1999, 9, 11)
-quiz_open = datetime(2026, 9, 10, 13, 24)
+quiz_open = datetime(2026, 9, 10, 13, 25)
 
 st.set_page_config(
     page_title="𑣲⋆ 27th Anniversary",
